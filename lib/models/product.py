b60afb15ddb80aff0b9a02d0ebedbf7e4a1ab889
maurice diong <mdiong00@gmail.com>
2024-02-19 18:34:00 -0500

    create save() method to save instance of Product created

--- lib/models/product.py
+++ lib/models/product.py
@@ -19,7 +19,7 @@ class Product:
     
     @classmethod
     def create_table(cls):
-        """create a products table"""
+        """create a products table to persist the attributes of Product instances"""
         sql = """
             CREATE TABLE IF NOT EXIST products(
             id INTEGER PRIMARY KEY,
@@ -33,4 +33,14 @@ class Product:
         CURSOR.execute(sql,)
         CONN.commit()
 
-    
+    def save(self):
+        """save the attribute of a product instance"""
+        sql = """
+            INSERT INTO products(name, description, quantity, price, brand_id)
+            VALUES(?,?,?,?,?)
+        """
+        CURSOR.execute(sql,(self.name, self.name, self.description, self.quantity, self.price, self.brand_id))
+        CONN.commit()
+        
+        self.id = CURSOR.lastrowid
+        type(self).all[self.id] = self
